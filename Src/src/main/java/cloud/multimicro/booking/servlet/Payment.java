cef5ed0c5990fde789cf9cc0b4d54e85a9db6cef
RANDRIANASOLO Jerimanjaka Tsiorisoa <tsiory@multimicro.fr>
2020-04-10 22:56:37 +0200

    ADD: appel WS payment et client avec servlet

--- Src/src/main/java/cloud/multimicro/booking/servlet/Payment.java
+++ Src/src/main/java/cloud/multimicro/booking/servlet/Payment.java
@@ -5,13 +5,20 @@
  */
 package cloud.multimicro.booking.servlet;
 
+import static com.sun.corba.se.spi.presentation.rmi.StubAdapter.request;
+import org.json.simple.JSONObject;
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Response;
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 
 /**
  *
@@ -31,7 +38,12 @@ public class Payment extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
+//        HttpSession session = request.getSession();
+//        session.setAttribute("mmcModeEncaissementId", "1");
+//        session.setAttribute("pmsNoteEnteteId", "1");
+//        session.setAttribute("mmcUserId", "1");
+//        session.setAttribute("montant", "12.3");
+//        response.setContentType("text/html;charset=UTF-8");
         getServletConfig().getServletContext().getRequestDispatcher("/payment.jsp").forward(request, response);
     }
 
@@ -47,6 +59,21 @@ public class Payment extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+//        HttpSession session = request.getSession();
+//        JSONObject paiment = new JSONObject();
+//        paiment.put("id","12");
+//        paiment.put("mmcModeEncaissementId", session.getAttribute("mmcModeEncaissementId"));
+//        paiment.put("pmsNoteEnteteId", session.getAttribute("pmsNoteEnteteId"));
+//        paiment.put("mmcUserId", session.getAttribute("mmcUserId"));
+//        paiment.put("montant", session.getAttribute("montant"));
+
+         JSONObject paiment = new JSONObject();
+        paiment.put("mmcModeEncaissementId", "1");
+        paiment.put("pmsNoteEnteteId", "1");
+        paiment.put("mmcUserId", "1");
+        paiment.put("montant", "2.3");
+        Payment.postPayment(paiment);
+        Payment.postClient(request);
         processRequest(request, response);
     }
 
@@ -74,4 +101,52 @@ public class Payment extends HttpServlet {
         return "Short description";
     }// </editor-fold>
 
+    private static void postPayment(JSONObject paiment) {
+        ResteasyClient client = new ResteasyClientBuilder().build();
+        ResteasyWebTarget target = client.target("http://localhost:8080/api/cashing/pms");
+        System.out.println(Entity.json(paiment));
+        Response response = target.request().header("Content-Type", "application/json").header("x-api-key", "CD19FD5E87DB2FB0056168D58D24753B42CC4B9B75894632242A2E2BA257402E").header("Authorization", "Bearer 5edc790b914878af26afd0f7cc56715028420006401f2a9f4d8d238b5c2beae7").post(Entity.json(paiment));
+        //Read output in string format
+        String value = response.readEntity(String.class);
+        response.close();
+    }
+
+    private static void postClient(HttpServletRequest request) {
+//        String code = request.getParameter("code");
+//        String nom = request.getParameter("nom");
+//        String prenom = request.getParameter("prenom");
+//        String adresse = request.getParameter("adresse");
+//        String ville = request.getParameter("ville");
+//        String codePostal = request.getParameter("codePostal");
+//        String telephone = request.getParameter("telephone");
+//        String email = request.getParameter("email");
+//        String pays = request.getParameter("pays");
+// clientObject.put("code", code);
+//        clientObject.put("nom", nom);
+//        clientObject.put("prenom", prenom);
+//        clientObject.put("adresse", adresse);
+//        clientObject.put("ville", ville);
+//        clientObject.put("codePostal", codePostal);
+//        clientObject.put("telephone", telephone);
+//        clientObject.put("email", email);
+//        clientObject.put("pays", pays);
+
+        ResteasyClient client = new ResteasyClientBuilder().build();
+        JSONObject clientObject = new JSONObject();
+        clientObject.put("code", "087");
+        clientObject.put("nom", "RANDRIANASOLO");
+        clientObject.put("prenom", "Tsiory");
+        clientObject.put("adresse", "Mada");
+        clientObject.put("ville", "Tana");
+        clientObject.put("codePostal", "250");
+        clientObject.put("telephone", "+23568956232");
+        clientObject.put("email", "tsiory@multimicro.fr");
+        clientObject.put("pays", "MG");
+
+        ResteasyWebTarget target = client.target("http://localhost:8080/api/clients");
+        Response response = target.request().header("Content-Type", "application/json").header("x-api-key", "CD19FD5E87DB2FB0056168D58D24753B42CC4B9B75894632242A2E2BA257402E").header("Authorization", "Bearer 5edc790b914878af26afd0f7cc56715028420006401f2a9f4d8d238b5c2beae7").post(Entity.json(clientObject));
+        //Read output in string format
+        String value = response.readEntity(String.class);
+        response.close();
+    }
 }
