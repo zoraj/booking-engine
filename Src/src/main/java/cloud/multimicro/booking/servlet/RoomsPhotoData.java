95b067aaf21a94a615a5fc8f58a6fab0c790001c
RAKOTONDRASOA Naly <naly@multimicro.fr>
2022-05-20 16:09:08 +0200

    MOD: Display slide image type chambre

---
+++ Src/src/main/java/cloud/multimicro/booking/servlet/RoomsPhotoData.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cloud.multimicro.booking.servlet;
+
+/**
+ *
+ * @author Tsiory-PC
+ */
+public class RoomsPhotoData {
+    private String listePhotoByRoomType;
+    
+    public RoomsPhotoData() {
+        
+    }
+
+    public String getListePhotoByRoomType() {
+        return listePhotoByRoomType;
+    }
+
+    public void setListePhotoByRoomType(String listePhotoByRoomType) {
+        this.listePhotoByRoomType = listePhotoByRoomType;
+    }
+    
+}
